1af41140dbb5de05405e69d5deb9e6aa96f63749
肖凡 <3004635919@qq.com>
2019-05-14 17:54:25 +0800

    modify

---
+++ src/Cav.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2019/5/14
+ * Time: 17:44
+ */
+namespace src;
+
+class Cav{
+    public function index(){
+        echo 'test';
+    }
+}
