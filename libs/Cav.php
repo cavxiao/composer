131b0f6f485a532081f25e8fe354ab6ecaf93b85
肖凡 <3004635919@qq.com>
2019-05-14 14:54:33 +0800

    fix

---
+++ libs/Cav.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2019/5/14
+ * Time: 14:52
+ */
+
+namespace test_com\libs;
+
+class Cav
+{
+    public function index()
+    {
+        echo 'this is desc';
+    }
+}
