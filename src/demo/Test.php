635a11e5c7a1b21d05c9ffc1b688618319dc1707
肖凡 <3004635919@qq.com>
2019-05-14 11:41:50 +0800

    初始化

---
+++ src/demo/Test.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2019/5/14
+ * Time: 11:21
+ */
+
+namespace src\demo;
+
+class Test
+{
+    public function __construct()
+    {
+        echo 'this is test' . '<pre>';
+    }
+
+    public function index()
+    {
+        echo 'this is index';
+    }
+}
